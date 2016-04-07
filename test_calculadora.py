1a185b0de8814fd56c5bd11ed14c7999e5317467
MaxOliveira <maxwell_oliveira2@hotmail.com>
2016-04-07 20:25:45 -0300

    Criando teste para subtracao e verificando que o teste quebra pois o metodo ainda nao existe

--- test_calculadora.py
+++ test_calculadora.py
@@ -4,3 +4,7 @@ from calculadora import ClassCalculadora
 def test_calculadora():
     calc = ClassCalculadora()
     assert calc.adicao(5,5) == 10
+
+def test_calculadora_subtracao():
+    calc = ClassCalculadora()
+    assert calc.subtracao(10, 4) == 6
